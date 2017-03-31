335b7b369903f4089d519e1799ab02b8360724bd
Ben Dunbar <dunbarb@ccs.neu.edu>
2017-03-31 17:33:12 -0400

    Removing junk folder. Renaming some directories.

---
+++ Magnet_Racer_Unity/Assets/Game/scripts/menu/playscreen.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playscreen : MonoBehaviour {
+    float delay = 3;
+    public AudioSource start_sound;
+
+    // Use this for initialization
+    void Update () {
+        delay -= Time.deltaTime;
+        if (delay <= 0)
+        {
+            Application.LoadLevel(3);
+            start_sound.Play();
+
+        }
+	}
+	
+	
+}
---
+++ Magnet_Racer_Unity/Assets/Game/scripts/tracklaptrigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tracklaptrigger : MonoBehaviour
+{
+
+    // next trigger in the lap
+    public tracklaptrigger next;
+
+    // when an object enters this trigger
+    void OnTriggerEnter2D(Collider2D other)
+    {
+      
+        lapcounter carLapCounter = other.gameObject.GetComponent<lapcounter>();
+        
+        if (carLapCounter)
+        {
+            Debug.Log("lap trigger " + gameObject.name);
+            carLapCounter.OnLapTrigger(this);
+            
+           
+        }
+    }
+}
