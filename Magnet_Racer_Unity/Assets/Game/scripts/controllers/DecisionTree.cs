9feaa1617b6555f937273a4546053e44bf5f6f56
Bill <wisheart.w@gmail.com>
2017-04-10 23:01:30 -0400

    Update

--- Magnet_Racer_Unity/Assets/Game/scripts/controllers/DecisionTree.cs
+++ Magnet_Racer_Unity/Assets/Game/scripts/controllers/DecisionTree.cs
@@ -6,9 +6,9 @@ public class DecisionTree : MonoBehaviour {
 
 	private Magnet magnetScript;
 
-	private float y1_slope = 0.446f;
+	private float y1_slope = 0.304f;
 	private float y_const = 1.25f;
-	private float y2_slope = -0.486f;
+	private float y2_slope = -0.55f;
 
 	void Start() {
 		magnetScript = GetComponent<Magnet> ();
