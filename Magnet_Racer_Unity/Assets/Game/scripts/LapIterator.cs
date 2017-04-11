45982d8dd14a63c8682264ecaeee5e0279b83467
Ben Dunbar <dunbarb@ccs.neu.edu>
2017-04-10 23:07:24 -0400

    Merge resolution

--- Magnet_Racer_Unity/Assets/Game/scripts/LapIterator.cs
+++ Magnet_Racer_Unity/Assets/Game/scripts/LapIterator.cs
@@ -7,15 +7,10 @@ public class LapIterator : MonoBehaviour
     public GateTrigger currentLapTrigger;
 	public TextMesh lapCountText;
 
-<<<<<<< HEAD:Magnet_Racer_Unity/Assets/Game/scripts/lapcounter.cs
-	TrackLapTrigger nextLapTrigger;
-	TrackLapTrigger winning;
-=======
     bool win = false;
 
 	GateTrigger nextLapTrigger;
 	GateTrigger winning;
->>>>>>> b2043d299c1abb0aebbba0777d0605a16182847f:Magnet_Racer_Unity/Assets/Game/scripts/LapIterator.cs
 
     int _lap;
    
