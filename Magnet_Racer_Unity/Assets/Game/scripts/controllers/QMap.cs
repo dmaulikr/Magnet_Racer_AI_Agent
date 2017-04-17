d4423e79f01f8e132b12802085bad2e67458d40d
Ben Dunbar <dunbarb@ccs.neu.edu>
2017-04-17 19:09:09 -0400

    Updates to scipts for q map. New folder for data

--- Magnet_Racer_Unity/Assets/Game/scripts/controllers/QMap.cs
+++ Magnet_Racer_Unity/Assets/Game/scripts/controllers/QMap.cs
@@ -1,16 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class QMap : MonoBehaviour {
 
+	private Dictionary<QState, int[]> qMap;
+
 	// Use this for initialization
 	void Start () {
-		
+		qMap = new Dictionary<QState, int[]> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	// read the stored q map.
+	void readDataFromFile(string fileName) {
+		int counter = 0;
+		string line;
+
+		// Read the file and display it line by line.
+		StreamReader file = 
+			new StreamReader(fileName);
+		while((line = file.ReadLine()) != null)
+		{
+			print (line);
+			counter++;
+		}
+
+		file.Close();
+
+	}
+
+	// Update the stored q map.
+	void writeDataToFile(string fileName) {
+		StreamWriter file = new StreamWriter (fileName, false);
+		{
+			string titleLine = "Data collection Date: " + System.DateTime.Now.ToString ();
+			file.WriteLine ("");file.WriteLine ("");
+			file.WriteLine (titleLine);
+			for(int i = 0; i < qMap.Keys.Count; i++) 
+			{
+				string line = "fake lin;";
+
+				file.WriteLine(line);
+			}
+		}
 	}
 }
