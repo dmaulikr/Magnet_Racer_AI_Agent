ee7014def226f58134ccabf9aad359fb91ebc10e
Ben Dunbar <dunbarb@ccs.neu.edu>
2017-04-17 23:03:10 -0400

    QState from Uttkarsh

--- Magnet_Racer_Unity/Assets/Game/scripts/controllers/QState.cs
+++ Magnet_Racer_Unity/Assets/Game/scripts/controllers/QState.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class QState {
+public class QState{
 
-	// Use to determine Quadrant!
-	private float y1_slope = 0.30f;
-	private float y_const = 1.25f;
-	private float y2_slope = -0.60f;
+    // Use to determine Quadrant!
+    private static float y1_slope = 0.30f;
+	private static float y_const = 1.25f;
+	private static float y2_slope = -0.60f;
 
 	// 0 or 1
 	private int thisCharge;
@@ -40,14 +40,57 @@ public class QState {
 		nearestOpponentCharge = otherCharge;
 	}
 
-	public static QState getQState(GameObject magnet, GameObject[] opponents) {
-		// TODO: implement. May need to add parameters
-		// Determine and set the 5 variables below based on positions and charges
-		int charge = 0;
-		int quadrant = 0;
-		int distance = 0;
-		int direction = 0;
-		int otherCharge = 0;
+	public QState getQState(GameObject magnet, GameObject[] opponents) {
+        // TODO: implement. May need to add parameters
+        // Determine and set the 5 variables below based on positions and charges
+      
+        Vector2 position = magnet.GetComponent<Magnet>().getPosition();
+        float x = position.x;
+        float y = position.y;
+
+        int charge = magnet.GetComponent<Magnet>().getCharge();
+
+        int quadrant = getQuadrant(x,y);
+
+
+        float distances, closest = float.MaxValue, xOpponent, yOpponent;
+        int locked = 0, dir;
+
+        for (int i = 0; i < 3; i++)
+            {
+                distances = Vector2.Distance(magnet.transform.position, opponents[i].transform.position);
+            
+                if (closest <= distances)
+                {
+                    closest = distances;
+                    locked = i;
+                }
+            }
+
+        int distance = (int) closest;
+
+
+            xOpponent = opponents[locked].transform.position.x;
+            yOpponent = opponents[locked].transform.position.y;
+            //simplified directions based on which quadrant the closest opponent is present
+            if (xOpponent > 0)
+            {
+                if (yOpponent > 0)
+                    dir = 0; //when the opponent is in +x,+y
+                else
+                    dir = 3; //when the opponent is in +x,-y
+            }
+            else
+            {
+                if (yOpponent > 0)
+                    dir = 1; //when the opponent is in  -x,+y
+                else
+                    dir = 2; //when the opponent is in -x,-y
+            }
+
+        int direction = dir; //simplified directions based on which quadrant the closest opponent is present
+
+		int otherCharge = opponents[locked].GetComponent<Magnet>().getCharge();
 
 		return new QState(charge, quadrant, distance, direction, otherCharge);
 	}
@@ -72,13 +115,47 @@ public class QState {
 		return thisCharge * 10000 + thisQuadrant * 1000 + nearestOpponentDistance * 100 + 
 			nearestOpponentDirection * 10 + nearestOpponentCharge;
 	}
-
-
+    
 	/*
 	 * Return 0 - 3 based on position in relation to quadrant lines y1 and y2.
 	 */ 
-	private static int getQuadrant(int x, int z) {
-		// TODO: implement.
-		return 0;
-	}
+	private int getQuadrant(float x, float y) {
+        
+        
+               
+        //If above y1
+        if (y > (y1_slope * x) + y_const)
+        {
+            //If above y2
+            if (y > (y2_slope * x) + y_const)
+            {
+                //In zone 1 gives 
+                return 0;
+            }
+            //If below y2
+            else
+            {
+                //In zone 4 gives
+                return 3;
+            }
+        }
+        //If below y1
+        else
+        {
+            //If above y2
+            if (y > (y2_slope * x) + y_const)
+            {
+                //In zone 2 gives
+                return 1;
+
+            }
+            //If below y2
+            else
+            {
+                //In zone 3
+                return 2;
+            }
+        }
+
+    }
 }
